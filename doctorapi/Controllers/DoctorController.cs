1be20a17a22f2fa5c3db8cc9ea84a87fd88a47e0
askawathekar <anandkawathekar20@gmail.com>
2021-07-07 16:56:34 +0530

    okay

---
+++ doctorapi/Controllers/DoctorController.cs
@@ -0,0 +1,29 @@
+﻿using DoctorAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoctorAPI.Controllers
+{
+   
+
+    [Route("DoctorController")]
+    [ApiController]
+    public class DoctorController : ControllerBase
+    {
+        DoctorDb database;
+
+        public DoctorController()
+        {
+            database = new DoctorDb();
+        }
+
+        [HttpGet]
+        public IActionResult GetAllDoctors()
+        {
+            return Ok(database);
+        }
+    }
+}
---
+++ doctorapi/Models/Doctor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoctorAPI.Models
+{
+    public class Doctor
+    {
+        public int DoctorID { get; set; }
+        public string DoctorName { get; set; }
+    }
+
+
+
+    public class DoctorDb : List<Doctor>
+    {
+        public DoctorDb()
+        {
+            Add(new Doctor() { DoctorID = 101, DoctorName = "Dr.Amol" });
+            Add(new Doctor() { DoctorID = 102, DoctorName = "Dr. Nishi" });
+        }
+    }
+}
